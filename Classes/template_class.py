c4fe9814ce33f821920c266d7353334920915044
GForb <48352959+GForb@users.noreply.github.com>
2025-11-13 18:42:03 +0000

    refactor so chat calls are now within chat class rather than template class

--- Classes/template_class.py
+++ Classes/template_class.py
@@ -1,4 +1,3 @@
-import time
 from pathlib import Path
 from docxtpl import DocxTemplate
 from Classes.chat_classes import OpenAIChat
@@ -9,40 +8,19 @@ class Template:
         template_path: str | Path,
         prompts_file,
         prompt_register,
-        *,
-        delay_sec: float = 0.01
     ) -> None:
 
         self.template_path = template_path
-        self.delay_sec = delay_sec # the delay used between calls to AI to prevent rate limits being exceeded.
         self.prompts_file = prompts_file
         self.prompt_register = prompt_register
 
     
-    def get_sap_content(self, protocol_text):
+    def get_sap_content(self, protocol_text, model = "gpt-5-2025-08-07"):
         chat_bot = OpenAIChat(
-            model_name = "gpt-5-2025-08-07", 
-            system_message = self.prompts_file.system_message(protocol_text))
-
-        results = {}
-        for var_name, prompt_func,  in self.prompt_register:
-            prompt = prompt_func()
-
-            print(f"Running {var_name}")
-            try:
-                response = chat_bot.get_response(prompt=prompt)
-                response_content = (response.get("content", "") or "").strip()
-                if not response_content:
-                    response_content = "ERROR"
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                response_content = "ERROR"
-            results[var_name] = response_content
-            
-            time.sleep(self.delay_sec)
+            model_name = model, 
+            system_message = self.system_message_function(protocol_text))
+        self.sap_content = chat_bot.run_prompts_register(prompt_register=self.prompt_register)
         
-        self.sap_content = results
-
     def save_content_as_text(self, path):
         sap_content = self.sap_content
         with open(path, "w") as f:
