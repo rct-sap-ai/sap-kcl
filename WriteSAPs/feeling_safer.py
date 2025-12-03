a9e69971413705093b0e8d334fd9fa0378185a67
GForb <48352959+GForb@users.noreply.github.com>
2025-12-03 13:14:35 +0000

    scripts to write SAPs

---
+++ WriteSAPs/feeling_safer.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from GenerateTemplates.generate_kcl_template import kcl_template_v02 as template
+
+template. write_sap(
+    protocol_path="Protocols/feeling_safer.pdf",  
+    sap_folder_path = "SAPs",
+    sap_name = "feeling_safer_v0.1",
+)
