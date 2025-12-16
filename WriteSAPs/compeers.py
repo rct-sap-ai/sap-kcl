bbe84a2a4a582e25b0f6784eba30b3c832f82299
GForb <48352959+GForb@users.noreply.github.com>
2025-12-16 21:13:52 +0000

    removing a space

--- WriteSAPs/compeers.py
+++ WriteSAPs/compeers.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from GenerateTemplates.generate_kcl_template import kcl_template_v02 as template
 
-template. write_sap(
+template.write_sap(
     protocol_path="Protocols/compeers.pdf",  
     sap_folder_path = "SAPs",
     sap_name = "compeers_v0.1",
--- WriteSAPs/feeling_safer.py
+++ WriteSAPs/feeling_safer.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from GenerateTemplates.generate_kcl_template import kcl_template_v02 as template
 
-template. write_sap(
+template.write_sap(
     protocol_path="Protocols/feeling_safer.pdf",  
     sap_folder_path = "SAPs",
     sap_name = "feeling_safer_v0.2",
