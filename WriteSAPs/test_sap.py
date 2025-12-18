e36b762e3ba7a4fd8f4479d731510c77fcbb226f
GForb <48352959+GForb@users.noreply.github.com>
2025-12-18 10:18:25 +0000

    removing an unused space

--- WriteSAPs/test_sap.py
+++ WriteSAPs/test_sap.py
@@ -1,7 +1,7 @@
 from auto_sap.generate_templates.generate_kcl_template import kcl_template_v02 as template
 
 
-template. write_sap(
+template.write_sap(
     protocol_path="Protocols/boppp.pdf",  
     sap_folder_path = "SAPs",
     sap_name = "boppp_v0.1_test",
